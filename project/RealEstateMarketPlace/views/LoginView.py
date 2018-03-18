d4bba5bce4e698930d49826ca0bbc7409c8ff9f4
MihMihai <mike_04_97@yahoo.ro>
2018-03-18 20:48:28 +0200

    Added List listings view + template. A lot of bootstrap needed here.

--- project/RealEstateMarketPlace/views/LoginView.py
+++ project/RealEstateMarketPlace/views/LoginView.py
@@ -4,6 +4,7 @@ from django import forms
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
+
 def LoginView(request):
     context = {}
 
@@ -24,7 +25,6 @@ def LoginView(request):
 
             else:
                 context['error'] = 'It was an error'
-
 
     context['form'] = form
 
