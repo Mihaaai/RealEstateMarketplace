36b8928a6634b631b76476ce659e0879ad906a98
MihMihai <mike_04_07@yahoo.ro>
2018-03-18 20:48:28 +0200

    Added List listings view + template. A lot of bootstrap needed here.

--- project/RealEstateMarketPlace/urls.py
+++ project/RealEstateMarketPlace/urls.py
@@ -4,10 +4,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path('',views.index ,name='default'),
+    path('', views.index, name='default'),
 
-	path('login/', views.LoginView, name='login'),
+    path('login/', views.LoginView, name='login'),
     path('logout/', views.LogoutView, name='logout'),
-    path('register/', views.RegisterView.as_view(), name='register')
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('add/', views.AddListingView, name='add'),
+    path('list/', views.ListListingsView, name='list_listings')
 ]
-
