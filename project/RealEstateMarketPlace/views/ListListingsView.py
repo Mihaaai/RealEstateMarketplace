8a7be799706048f68542082710b09cbd78416989
MihMihai <mike_04_07@yahoo.ro>
2018-03-19 19:07:24 +0200

    Changed simple view for List Listings to Generic View: ListView

--- project/RealEstateMarketPlace/views/ListListingsView.py
+++ project/RealEstateMarketPlace/views/ListListingsView.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic.list import ListView
 from ..models import Listing
 
 # Create your views here.
 
 
-def ListListingsView(request):
-    context = {}
-    context['listings'] = Listing.objects.all()
-    return render(request, 'list_listings_template.html', context)
+class ListListingsView(ListView):
+    template_name = 'list_listings_template.html'
+    model = Listing
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
