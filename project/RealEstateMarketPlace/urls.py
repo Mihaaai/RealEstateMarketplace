2bae0f48429d5cb12cb6c007b74f18037814863a
Mihaaai <ghidome@gmail.com>
2018-03-22 23:14:31 +0200

    Added Detail View and Delete View for a listing, along with dummy templates

--- project/RealEstateMarketPlace/urls.py
+++ project/RealEstateMarketPlace/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='default'),
+    path('', views.index, name='homepage'),
 
     path('login/', views.LoginView, name='login'),
     path('logout/', views.LogoutView, name='logout'),
@@ -12,4 +12,7 @@ urlpatterns = [
     path('add/', views.AddListingView.as_view(), name='add'),
     path('list/', views.ListListingsView.as_view(), name='list_listings'),
     path('profile/<int:pk>/my-listings/', views.ListMyListingsView.as_view(), name='list_my_listings'),
+    path('list/<int:pk>',views.DetailListingView.as_view(),name="details_listing"),
+    path('list/<int:pk>/delete',views.DeleteListingView.as_view(),name="delete_listing"),
+    path('favorite/',views.AddFavoriteListingView.as_view(),name="add_favorite_listing")
 ]
