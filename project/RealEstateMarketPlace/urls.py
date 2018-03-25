891e0b16dfb6e73173824f1dbd9a75b29456631f
MihMihai <mike_04_97@yahoo.ro>
2018-03-25 19:34:51 +0300

    Added more options to search functionality + tweaked Add to favorites

--- project/RealEstateMarketPlace/urls.py
+++ project/RealEstateMarketPlace/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('list/my-listings/', views.ListMyListingsView.as_view(), name='list_my_listings'),
     path('list/<int:pk>', views.DetailListingView.as_view(), name='details_listing'),
     path('list/<int:pk>/delete', views.DeleteListingView.as_view(), name='delete_listing'),
-    path('list/<listing_id>/favorite', views.add_favorite_listing, name='favorite'),
+    path('list/<listing_id>/favorite', views.AddFavoriteListingAPI.as_view(), name='favorite'),
     path('list/favorites', views.ListMyFavoriteListingsView.as_view(), name='fav_listings'),
     path('list/search/', views.SearchListListingsView.as_view(), name='search_listings'),
 
