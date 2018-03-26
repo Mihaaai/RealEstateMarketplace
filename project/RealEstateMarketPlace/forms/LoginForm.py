2e82674517a9e3507626396b670deb1e4bce8ac1
Bogdan <bogdanalexandrescu96@gmail.com>
2018-03-26 20:41:49 +0000

    added style for forms. create form_template

--- project/RealEstateMarketPlace/forms/LoginForm.py
+++ project/RealEstateMarketPlace/forms/LoginForm.py
@@ -3,8 +3,8 @@ from ..models import User
 
 
 class LoginForm(forms.Form):
-    email = forms.CharField(label="Email", required=True)
-    password = forms.CharField(label="Password", widget=forms.PasswordInput, required=True)
+    email = forms.CharField(widget=forms.EmailInput(), label="Email", required=True)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(), required=True)
 
     class Meta:
         model = User
