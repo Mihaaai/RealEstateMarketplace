57573a39a251ded6dbd65b6e976c2e2cbeb751b4
Mihaaai <ghidome@gmail.com>
2018-04-03 17:49:13 +0300

    Added rest of Update Listing. For now, closing a listing requires entering its update page, but in the future, having a separate button for closing a listing would be nice

---
+++ project/RealEstateMarketPlace/forms/UpdateListingForm.py
@@ -0,0 +1,51 @@
+from ..forms import AddListingForm
+
+from django import forms
+from string import Template
+from django.utils.safestring import mark_safe
+
+
+class PictureWidget(forms.widgets.ClearableFileInput):
+	def render(self, name, value, attrs=None, renderer = None):
+		html = ""
+
+		# if the imageField has not an image yet 
+		if value is None:
+			html += """
+			Currently empty<br>
+			Add :
+			"""
+		# else show the current
+		else:
+			html += """
+			Currently: <img height="200px" width="200px" src="$link" alt="$link_name">
+			<input type="checkbox" name="image-clear" id="image-clear_id">
+			<label for="image-clear_id">Clear</label><br>
+			Change:
+		"""
+
+		html += """
+			<input type="file" name="image" class="form-control" id="id_image">
+		"""
+		
+		if value is not None:
+			html = Template(html)
+			html = html.substitute(link = value.url,link_name = value.url.replace('media/',''))
+
+		return mark_safe(html)
+
+
+class UpdateListingForm(AddListingForm):
+
+	class Meta(AddListingForm.Meta):
+		fields = ['title', 'description', 'user_id', 'is_closed']	
+		
+	# by assigning a picture widget, we have a preview of the current image
+	image = forms.ImageField(required=False, widget = PictureWidget)
+		
+
+
+		
+
+
+
---
+++ project/RealEstateMarketPlace/views/UpdateListingView.py
@@ -0,0 +1,31 @@
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from ..forms import UpdateListingForm
+from ..models import Listing
+
+class UpdateListingView(LoginRequiredMixin, UpdateView):
+	login_url = reverse_lazy('login')
+	success_url = reverse_lazy('list_listings')
+	form_class = UpdateListingForm
+	template_name = 'update_listing_template.html'
+	model = Listing
+
+	def get_initial(self):
+
+		listing = self.get_object()
+		estate = listing.estate_id
+
+		return {
+			'user_id': self.request.user.id,
+			'city': estate.city,
+			'image': estate.image,
+			'rooms': estate.rooms,
+			'bathrooms': estate.rooms,
+			'floor': estate.floor,
+			'size': estate.size,
+			'year': estate.year,
+			'price': estate.price
+		}
+
