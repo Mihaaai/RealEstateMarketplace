5e8823b3708001f8bf001efca87abffe675e0d6c
MihMihai <mike_04_97@yahoo.ro>
2018-04-14 14:08:41 +0300

    Moved css styles from listing details template to details.css. Added styles in order to make the page responsive

--- project/RealEstateMarketPlace/forms/AddListingForm.py
+++ project/RealEstateMarketPlace/forms/AddListingForm.py
@@ -5,38 +5,38 @@ from ..models import (Estate, Listing, User, NEIGHBORHOOD_CHOICES, PARTITIONING_
 
 class AddListingForm(forms.ModelForm):
 
-	class Meta:
-		model = Listing
-		fields = ['title', 'description', 'user_id', ]
+    class Meta:
+        model = Listing
+        fields = ['title', 'description', 'user_id', ]
 
-	user_id = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.HiddenInput)
+    user_id = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.HiddenInput)
 
-	image = forms.ImageField(required=False)
-	neighborhood = forms.ChoiceField(choices=NEIGHBORHOOD_CHOICES, required=True)
-	partitioning = forms.ChoiceField(choices=PARTITIONING_CHOICES, required=True)
-	rooms = forms.IntegerField(required=True, min_value=1, max_value=10)
-	bathrooms = forms.IntegerField(required=True, min_value=0, max_value=5)
-	floor = forms.IntegerField(required=True, min_value=0, max_value=20)
-	size = forms.FloatField(required=True, min_value=10, max_value=200)
-	year = forms.IntegerField(required=True, min_value=1900, max_value=2018)
-	price = forms.FloatField(required=True, min_value=0)
+    image = forms.ImageField(required=False)
+    neighborhood = forms.ChoiceField(choices=NEIGHBORHOOD_CHOICES, required=True)
+    partitioning = forms.ChoiceField(choices=PARTITIONING_CHOICES, required=True)
+    rooms = forms.IntegerField(required=True, min_value=1, max_value=10)
+    bathrooms = forms.IntegerField(required=True, min_value=0, max_value=5)
+    floor = forms.IntegerField(required=True, min_value=0, max_value=20)
+    size = forms.FloatField(required=True, min_value=10, max_value=200)
+    year = forms.IntegerField(required=True, min_value=1900, max_value=2018)
+    price = forms.FloatField(required=True, min_value=0)
 
-	@transaction.atomic
-	def save(self):
-		estate = Estate(neighborhood=self.cleaned_data['neighborhood'],
-		                partitioning=self.cleaned_data['partitioning'],
-						rooms=self.cleaned_data['rooms'],
-						floor=self.cleaned_data['floor'],
-						size=self.cleaned_data['size'],
-						year=self.cleaned_data['year'],
-						price=self.cleaned_data['price'],
-						image=self.cleaned_data['image'],
-						bathrooms=self.cleaned_data['bathrooms'],
-						)
-		estate.save()
+    @transaction.atomic
+    def save(self):
+        estate = Estate(neighborhood=self.cleaned_data['neighborhood'],
+                        partitioning=self.cleaned_data['partitioning'],
+                        rooms=self.cleaned_data['rooms'],
+                        floor=self.cleaned_data['floor'],
+                        size=self.cleaned_data['size'],
+                        year=self.cleaned_data['year'],
+                        price=self.cleaned_data['price'],
+                        image=self.cleaned_data['image'],
+                        bathrooms=self.cleaned_data['bathrooms'],
+                        )
+        estate.save()
 
-		listing = super().save(commit=False)
-		listing.estate_id = estate
-		listing.save()
+        listing = super().save(commit=False)
+        listing.estate_id = estate
+        listing.save()
 
-		return listing
+        return listing
