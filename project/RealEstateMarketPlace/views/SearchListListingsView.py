0171d2144e52a72790cd22f06379527a91685d75
MihMihai <mike_04_97@yahoo.ro>
2018-05-08 22:17:07 +0300

    Added some css to image in Details page. Fixed table border in Details page and buttons too close to each other. Replaced menu icon with search icon. Replaced city with neighborhood. Fixed a bug where you could not search for listings if you didn't specify the number of rooms. Moved js code to files

--- project/RealEstateMarketPlace/views/SearchListListingsView.py
+++ project/RealEstateMarketPlace/views/SearchListListingsView.py
@@ -11,16 +11,20 @@ class SearchListListingsView(ListListingsView):
         # http://127.0.0.1:8000/list/search/?rooms=5&minPrice=200&maxPrice=300&minYear=1997&maxYear=2018&city=Bucuresti
         rooms = self.request.GET.get('rooms') if self.request.GET.get('rooms') else 0
         minPrice = self.request.GET.get('minPrice') if self.request.GET.get('minPrice') else 0
-        maxPrice = self.request.GET.get('maxPrice') if self.request.GET.get('maxPrice') else 99999999
+        maxPrice = self.request.GET.get('maxPrice') if self.request.GET.get('maxPrice') else 9999999999
         minYear = self.request.GET.get('minYear') if self.request.GET.get('minYear') else 0
         maxYear = self.request.GET.get('maxYear') if self.request.GET.get('maxYear') else 9999
-        city = self.request.GET.get('city') if self.request.GET.get('city') else ""
+        neighborhood = self.request.GET.get('neighborhood') if self.request.GET.get('neighborhood') else ""
         search_keyword = self.request.GET.get('q') if self.request.GET.get('q') else ""
 
-        return Listing.objects.filter(is_closed=False) \
-            .filter(estate_id__rooms=rooms) \
+        query = Listing.objects.filter(is_closed=False) \
             .filter(estate_id__price__range=(minPrice, maxPrice)) \
             .filter(estate_id__year__range=(minYear, maxYear)) \
-            .filter(estate_id__city__contains=city) \
+            .filter(estate_id__neighborhood__contains=neighborhood) \
             .filter(title__contains=search_keyword) \
             .order_by('-created')
+
+        if rooms != 0:
+            query.filter(estate_id__rooms=rooms)
+
+        return query
